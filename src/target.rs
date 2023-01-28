6e0ab8da4aebfc2ffd8bb65715f6f9eaf2e7b32a
823984418 <823984418@qq.com>
2023-01-28 22:59:51 +0800

    inline

--- src/target.rs
+++ src/target.rs
@@ -28,18 +28,22 @@ pub struct StrongRef<'gc, T: ?Sized + NodeTrait<'gc> + 'gc> {
 }
 
 impl<'gc, T: ?Sized + NodeTrait<'gc> + 'gc> StrongRef<'gc, T> {
+    #[inline(always)]
     pub fn get(&self) -> Option<RootRef<'gc, T>> {
         self.cell.get().map(|r| unsafe { RootRef::new(r.as_ref()) })
     }
 
+    #[inline(always)]
     pub fn set(&self, r: Option<&T>) {
         self.cell.set(r.map(Into::into));
     }
 
+    #[inline(always)]
     pub fn set_ref(&self, r: &T) {
         self.cell.set(Some(NonNull::from(r)));
     }
 
+    #[inline(always)]
     pub fn set_none(&self) {
         self.cell.set(None);
     }
@@ -58,12 +62,15 @@ impl<'gc, T: ?Sized + NodeTrait<'gc>> Debug for StrongRef<'gc, T> {
 }
 
 unsafe impl<'gc, T: ?Sized + NodeTrait<'gc>> RefSet<'gc> for StrongRef<'gc, T> {
+    #[inline(always)]
     unsafe fn build() -> Self {
         Self {
             _marker: PhantomData,
             cell: Cell::new(None),
         }
     }
+    
+    #[inline(always)]
     unsafe fn collect(&self, stack: &mut Vec<&dyn NodeTrait<'gc>>, max: State) {
         if let Some(r) = self.cell.get() {
             let r = r.as_ref();
@@ -76,10 +83,12 @@ unsafe impl<'gc, T: ?Sized + NodeTrait<'gc>> RefSet<'gc> for StrongRef<'gc, T> {
 }
 
 unsafe impl<'gc, T: RefSet<'gc>, const N: usize> RefSet<'gc> for [T; N] {
+    #[inline(always)]
     unsafe fn build() -> Self {
         from_fn(|_| T::build())
     }
 
+    #[inline(always)]
     unsafe fn collect(&self, stack: &mut Vec<&dyn NodeTrait<'gc>>, max: State) {
         for i in self {
             i.collect(stack, max);
@@ -88,28 +97,34 @@ unsafe impl<'gc, T: RefSet<'gc>, const N: usize> RefSet<'gc> for [T; N] {
 }
 
 unsafe impl<'gc> RefSet<'gc> for () {
+    #[inline(always)]
     unsafe fn build() -> Self {
         ()
     }
 
+    #[inline(always)]
     unsafe fn collect(&self, stack: &mut Vec<&dyn NodeTrait<'gc>>, _max: State) {}
 }
 
 unsafe impl<'gc, A: RefSet<'gc>> RefSet<'gc> for (A,) {
+    #[inline(always)]
     unsafe fn build() -> Self {
         (A::build(),)
     }
 
+    #[inline(always)]
     unsafe fn collect(&self, stack: &mut Vec<&dyn NodeTrait<'gc>>, max: State) {
         self.0.collect(stack, max);
     }
 }
 
 unsafe impl<'gc, A: RefSet<'gc>, B: RefSet<'gc>> RefSet<'gc> for (A, B) {
+    #[inline(always)]
     unsafe fn build() -> Self {
         (A::build(), B::build())
     }
 
+    #[inline(always)]
     unsafe fn collect(&self, stack: &mut Vec<&dyn NodeTrait<'gc>>, max: State) {
         self.0.collect(stack, max);
         self.1.collect(stack, max);
