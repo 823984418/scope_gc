d3eed0bfcbc9788ce424bed1aa12faae83291441
823984418 <823984418@qq.com>
2023-01-29 22:04:57 +0800

    tuple

--- src/struct_ref.rs
+++ src/struct_ref.rs
@@ -50,3 +50,85 @@ unsafe impl<'gc, A: RefSet<'gc>, B: RefSet<'gc>> RefSet<'gc> for (A, B) {
         self.1.collect(stack);
     }
 }
+
+unsafe impl<'gc, A: RefSet<'gc>, B: RefSet<'gc>, C: RefSet<'gc>> RefSet<'gc> for (A, B, C) {
+    #[inline(always)]
+    unsafe fn build() -> Self {
+        (A::build(), B::build(), C::build())
+    }
+
+    #[inline(always)]
+    unsafe fn collect(&self, stack: &mut Vec<&dyn NodeTrait<'gc>>) {
+        self.0.collect(stack);
+        self.1.collect(stack);
+        self.2.collect(stack);
+    }
+}
+
+unsafe impl<'gc, A: RefSet<'gc>, B: RefSet<'gc>, C: RefSet<'gc>, D: RefSet<'gc>> RefSet<'gc>
+    for (A, B, C, D)
+{
+    #[inline(always)]
+    unsafe fn build() -> Self {
+        (A::build(), B::build(), C::build(), D::build())
+    }
+
+    #[inline(always)]
+    unsafe fn collect(&self, stack: &mut Vec<&dyn NodeTrait<'gc>>) {
+        self.0.collect(stack);
+        self.1.collect(stack);
+        self.2.collect(stack);
+        self.3.collect(stack);
+    }
+}
+
+unsafe impl<'gc, A: RefSet<'gc>, B: RefSet<'gc>, C: RefSet<'gc>, D: RefSet<'gc>, E: RefSet<'gc>>
+    RefSet<'gc> for (A, B, C, D, E)
+{
+    #[inline(always)]
+    unsafe fn build() -> Self {
+        (A::build(), B::build(), C::build(), D::build(), E::build())
+    }
+
+    #[inline(always)]
+    unsafe fn collect(&self, stack: &mut Vec<&dyn NodeTrait<'gc>>) {
+        self.0.collect(stack);
+        self.1.collect(stack);
+        self.2.collect(stack);
+        self.3.collect(stack);
+        self.4.collect(stack);
+    }
+}
+
+unsafe impl<
+        'gc,
+        A: RefSet<'gc>,
+        B: RefSet<'gc>,
+        C: RefSet<'gc>,
+        D: RefSet<'gc>,
+        E: RefSet<'gc>,
+        F: RefSet<'gc>,
+    > RefSet<'gc> for (A, B, C, D, E, F)
+{
+    #[inline(always)]
+    unsafe fn build() -> Self {
+        (
+            A::build(),
+            B::build(),
+            C::build(),
+            D::build(),
+            E::build(),
+            F::build(),
+        )
+    }
+
+    #[inline(always)]
+    unsafe fn collect(&self, stack: &mut Vec<&dyn NodeTrait<'gc>>) {
+        self.0.collect(stack);
+        self.1.collect(stack);
+        self.2.collect(stack);
+        self.3.collect(stack);
+        self.4.collect(stack);
+        self.5.collect(stack);
+    }
+}
